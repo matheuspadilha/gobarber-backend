73e7adc94c4ed508aab8d9b8507041056b70cfb7
Matheus Padilha <mvpadilha94@gmail.com>
2020-07-21 23:24:43 -0300

    adcionando sistema de cache com redis e controlhando a quantidade de conexões por segundo

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
-    expiresIn: process.env.EXPIRES_IN,
+    secret: process.env.APP_SECRET || 'default',
+    expiresIn: process.env.EXPIRES_IN || '1d',
   },
 };
